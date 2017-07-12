a4470714dc8a8856e42c15f582b3d6c0d4af4d5d
Charles Tilford <charles.tilford@bms.com>
2017-07-12 15:32:46 -0400

    Prior to recovering remote

--- stylish/dumpStylishStyles.pl
+++ stylish/dumpStylishStyles.pl
@@ -9,13 +9,14 @@ use DBD::SQLite;
 my $workDir = dirname($0);
 my $ffDir   = `\$HOME/confFiles/systemSetup/findFirefoxProfile.sh 1`;
 $ffDir      =~ s/[\n\r]+$//;
+my $db      = "/home/tilfordc/firefoxProfile/stylish.sqlite";
 
-warn "
+print "
 Working Directory: $workDir
-Firefox Profile: $ffDir
+  Firefox Profile: $ffDir
+         Database: $db
 ";
 
-my $db = "/home/tilfordc/firefoxProfile/stylish.sqlite";
 
 die "Failed to locate Stylish SQLite database at:\n  $db\n" unless (-s $db);
 
@@ -25,14 +26,45 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=$db",'','', {
     RaiseError => 1,
     PrintError => 0 });
 
-my $get = $dbh->prepare("SELECT name, code FROM styles");
+
+# id will be specific to each SQLite instance, do not include
+my @metaCols = qw(url updateUrl md5Url enabled originalCode 
+                  idUrl applyBackgroundUpdates originalMd5);
+my $get = $dbh->prepare("SELECT code, name, ".
+                        join(', ', @metaCols)." FROM styles ORDER BY name");
+my $xFile = "$workDir/_extracol.tsv";
+my $mFile = "$workDir/_metadata.tsv";
+open(XFILE, ">$xFile") || die "Failed to write extra col info\n  $xFile\n  $!";
+print XFILE join("\t", 'name', @metaCols)."\n";
 $get->execute();
 my $rows = $get->fetchall_arrayref();
-foreach my $row (@{$rows}) {
-    my ($name, $css) = @{$row};
+for my $r (0..$#{$rows}) {
+    my @line = map { defined $_ ? $_ : '{null}' } @{$rows->[$r]};
+    my $code = shift @line;
+    my $name = $line[0];
     my $file = "$workDir/$name.css";
     open(OUT, ">$file") || die "Failed to write file\n  $file\n  $!";
-    print OUT $css;
+    print OUT $code;
     close OUT;
+    map { s/\t/    /g } @line;
+    print XFILE join("\t", @line)."\n";
+    print "\n" unless ($r % 4);
+    printf("%5d %-24s", (-s $file), "$name.css");
 }
-print `ls -lh "$workDir"`;
+close XFILE;
+
+my $mget = $dbh->prepare("SELECT s.name, m.name, m.value". 
+                         "  FROM styles s, style_meta m".
+                         " WHERE s.id = m.style_id ORDER BY s.name, m.name");
+$mget->execute();
+my $mrows = $mget->fetchall_arrayref();
+open(MFILE, ">$mFile") || die "Failed to write metadata\n  $mFile\n  $!";
+print MFILE join("\t", qw(styleName name value))."\n";
+foreach my $row (@{$mrows}) {
+    my @line = map { defined $_ ? $_ : '{null}' } @{$row};
+    map { s/\t/    /g } @line;
+    print MFILE join("\t", @line)."\n";
+}
+
+print "\n\nExtraCols: $xFile\n Metadata: $mFile\n\n";
+# print `ls -lh "$workDir"`;
