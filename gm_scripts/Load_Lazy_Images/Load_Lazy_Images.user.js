2c42d0b52aad04510f84b963a4635e03ca5e20cb
Charles Tilford <charles.tilford@bms.com>
2017-07-17 10:04:07 -0400

    More GM scripts

--- gm_scripts/Load_Lazy_Images/Load_Lazy_Images.user.js
+++ gm_scripts/Load_Lazy_Images/Load_Lazy_Images.user.js
@@ -2,6 +2,8 @@
 // @name        Load_Lazy_Images
 // @namespace   TOR
 // @include     https://*washingtonpost.com/*
+// @include     https://www.nytimes.com*
+// @include     http://www.space.com/*
 // @version     1
 // @grant       none
 // @grant       GM_log
@@ -9,10 +11,23 @@
 
 var imgs = document.getElementsByTagName('img');
 var ilen = imgs.length;
+var loc  = document.location;
 
-by_attr();
+if (/washingtonpost/.test(loc)) {
+    hiResSrc();
+} else {
+    dataSrc();
+}
 
-function by_attr() {
+function dataSrc() {
+    for (var i = 0; i < ilen; i++) {
+        var img  = imgs[i];
+        var ds   = img.getAttribute( 'data-src' );
+        if (ds) img.src = ds;
+    }
+}
+
+function hiResSrc() {
     for (var i = 0; i < ilen; i++) {
         var img  = imgs[i];
         var dhrs = img.getAttribute( 'data-hi-res-src' );
