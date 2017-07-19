789a66957c5642a98d1c71807a3c1c791326d0c6
Charles Tilford <github@biocode.fastmail.fm>
2017-07-19 06:45:20 -0400

    Reuters lazy-load

--- gm_scripts/Load_Lazy_Images/Load_Lazy_Images.user.js
+++ gm_scripts/Load_Lazy_Images/Load_Lazy_Images.user.js
@@ -4,6 +4,7 @@
 // @include     https://*washingtonpost.com/*
 // @include     https://www.nytimes.com*
 // @include     http://www.space.com/*
+// @include     https://*reuters.com/*
 // @version     1
 // @grant       none
 // @grant       GM_log
@@ -12,10 +13,16 @@
 var imgs = document.getElementsByTagName('img');
 var ilen = imgs.length;
 var loc  = document.location;
+var bgChk = new RegExp('(&w=[0-9]+)"');
 
 if (/washingtonpost/.test(loc)) {
+    /* non-standard attributes plus a blur style */
     hiResSrc();
+} else if (/reuters/.test(loc)) {
+    /* Using a background-image CSS style to define image source */
+    divBackground();
 } else {
+    /* This seems like the 'standard' way to do a lazy load */
     dataSrc();
 }
 
@@ -38,6 +45,21 @@ function hiResSrc() {
             img.style.filter = "unset";
             // console.log(img.className +" : Source = " + dhrs);
             continue;
+        }
+    }
+}
+
+function divBackground() {
+    var divs = document.getElementsByTagName('div');
+    var dlen = divs.length;
+    for (var d = 0; d < dlen; d++) {
+        var div  = divs[d];
+        var bg   = div.style["background-image"];
+        if (!bg) continue;
+        var bgm  = bg.match(bgChk);
+        if (bgm && bgm[1]) {
+            w = bgm[1];
+            div.style["background-image"] = bg.replace(w, "&w=1280");
         }
     }
 }
