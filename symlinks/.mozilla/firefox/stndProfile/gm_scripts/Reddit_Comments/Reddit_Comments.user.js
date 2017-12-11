28e6500243ae13f0113e8dc9736a0913961cfd73
Charles Tilford <gitcommit@biocode.fastmail.fm>
2017-12-11 06:28:41 -0500

    Markup landing pages - votes / comment count

--- symlinks/.mozilla/firefox/stndProfile/gm_scripts/Reddit_Comments/Reddit_Comments.user.js
+++ symlinks/.mozilla/firefox/stndProfile/gm_scripts/Reddit_Comments/Reddit_Comments.user.js
@@ -17,40 +17,50 @@ setTimeout(highlight, 3000);
 // repoint_r_news();
 
 function highlight() {
-    // Sometimes spans, sometimes divs
-    var elems = [ document.getElementsByTagName('div'),
-		  document.getElementsByTagName('span') ];
-    log("Scanning " + elems[0].length + " DIVs + "+ elems[1].length + " SPANs");
+    // Sometimes spans, sometimes divs. Links for index comment count
+    
+    var elems = [ [document.getElementsByTagName('div'),
+    {"Comment__metadata": 1, "score unvoted": 1}],
+		  [document.getElementsByTagName('span'),
+    {"score unvoted": 1, "score-hidden": 1}],
+		  [document.getElementsByTagName('a'),
+    {"bylink comments may-blank": 1}]
+                  ];
+    log("Scanning " + elems[0][0].length + " DIVs + "+
+        elems[1][0].length + " SPANs");
     for (var e = 0; e < elems.length; e++) {
-	var e2 = elems[e];
+	var e2 = elems[e][0];
 	var e2l = e2.length;
+        var okClass = elems[e][1];
 	for (var s = 0; s < e2l; s++) {
             var elem = e2[s];
             var cn = elem.className;
 	    var tn = elem.tagName.toLowerCase();
-            if ((tn == 'div' && cn != "Comment__metadata") ||
-		(tn == 'span' && cn != "score unvoted" &&
-                 cn != 'score-hidden')) continue;
+            if (!okClass[ cn ]) continue;
             var pts = elem.innerHTML;
             var col = "";
+	    var fgc = "";
             // log(pts + ' = ' + col);
 	    pts = pts.replace(/ points?.*/,'');
+	    pts = pts.replace(/ comments?.*/,'');
             if (pts == '[score hidden]') {
 		col = 'silver';
             } else if (/\dk$/.test(pts)) {
-		elem.style.color = 'yellow';
+		fgc = 'yellow';
 		col = 'purple';
             } else {
 		pts = pts.replace(',','');
 		if (pts >=  1000) {
 		    col = 'purple';
-		    elem.style.color = 'white';
+		    fgc = 'white';
 		} else if (pts >= 100) {
 		    col = 'lime';
+		    fgc = 'black';
 		} else if (pts >= 50) {
 		    col = 'yellow';
 		} else if (pts < -100) {
 		    col = 'black';
+		    fgc = 'white';
 		} else if (pts <= 5) {
 		    var par = elem;
 		    while (par && par.tagName != 'div' && 
@@ -60,25 +70,12 @@ function highlight() {
 		    if (par) par.style.display = 'none';
 		}
             }
-            if (col != "") {
-		elem.style.backgroundColor = col;
-		//log(pts + ' = ' + col);
-            }
+            if (col != "")  elem.style.backgroundColor = col;
+            if (fgc != "")  elem.style.color = fgc;
 	}
     }
 }
 
 function log (msg) {
-    unsafeWindow.console.log(msg)
-}
-
-function repoint_r_news () {
-    var links = document.getElementsByTagName('a');
-    var llen = links.length;
-    for (var i = 0; i < llen; i++ ) {
-       var link = links[i];
-       if (link.href == "https://www.reddit.com/r/news/") {
-           link.href = "https://www.reddit.com/r/uncensorednews";
-       }
-    }
+    unsafeWindow.console.log(msg);
 }
