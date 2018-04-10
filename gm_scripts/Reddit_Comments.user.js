b291ee57f04bfd194426ea019a94538ba3112b24
Charles Tilford <gitcommit@biocode.fastmail.fm>
2018-04-09 20:54:21 -0400

    Reddit has drastically changed their DOM

--- gm_scripts/Reddit_Comments.user.js
+++ gm_scripts/Reddit_Comments.user.js
@@ -10,44 +10,33 @@
 // @include        https://www.reddit.com/user/*
 // @include        https://www.reddit.com/r/*
 // @include        https://www.reddit.com/
-// @version        1.0.1
+// @version        1.1
 // @grant          none
 // ==/UserScript==
-log("-- Reddit Comment Highlighter --");
+console.log("-- Reddit Comment Highlighter --");
 
 setTimeout(highlight, 3000);
 // repoint_r_news();
 
 function highlight() {
     // Sometimes spans, sometimes divs. Links for index comment count
-    
-    var elems = [ [document.getElementsByTagName('div'),
-    {"Comment__metadata": 1, "score unvoted": 1}],
-		  [document.getElementsByTagName('span'),
-    {"score unvoted": 1, "score-hidden": 1}],
-		  [document.getElementsByTagName('a'),
-    {"bylink comments may-blank": 1}]
-                  ];
-    log("Scanning " + elems[0][0].length + " DIVs + "+
-        elems[1][0].length + " SPANs");
-    for (var e = 0; e < elems.length; e++) {
-	var e2 = elems[e][0];
-	var e2l = e2.length;
-        var okClass = elems[e][1];
-	for (var s = 0; s < e2l; s++) {
-            var elem = e2[s];
-            var cn = elem.className;
-	    var tn = elem.tagName.toLowerCase();
-            if (!okClass[ cn ]) continue;
-            var pts = elem.innerHTML;
-            var col = "";
-	    var fgc = "";
-            // log(pts + ' = ' + col);
+
+    // YEEAARRGGG. 9 April 2018 - New, heavily-obfuscated DOM structure
+
+    var spans = document.getElementsByTagName('span');
+    var sNum  = spans.length;
+    console.log("Scanning " + sNum + " SPANs");
+    for (var e = 0; e < spans.length; e++) {
+        var elem = spans[e];
+        var pts  = elem.innerText.toLowerCase();
+        var col = "";
+        var fgc = "";
+        if (pts == 'score hidden') {
+            col = 'silver';
+        } else {
 	    pts = pts.replace(/ points?.*/,'');
 	    pts = pts.replace(/ comments?.*/,'');
-            if (pts == '[score hidden]') {
-		col = 'silver';
-            } else if (/\dk$/.test(pts)) {
+            if (/\dk$/.test(pts)) {
 		fgc = 'yellow';
 		col = 'purple';
             } else {
@@ -64,20 +53,17 @@ function highlight() {
 		    col = 'black';
 		    fgc = 'white';
 		} else if (pts <= 5) {
-		    var par = elem;
-		    while (par && par.tagName != 'div' && 
-			   par.className != 'entry unvoted') {
-			par = par.parentNode;
-		    }
+		    var par = elem.parentNode.parentNode;
 		    if (par) par.style.display = 'none';
 		}
             }
-            if (col != "")  elem.style.backgroundColor = col;
-            if (fgc != "")  elem.style.color = fgc;
-	}
+        }
+        if (col != "")  elem.style.backgroundColor = col;
+        if (fgc != "")  elem.style.color = fgc;
     }
-}
 
-function log (msg) {
-    unsafeWindow.console.log(msg);
+    var links = document.getElementsByTagName('a');
+    for (var l = 0; l < links.length; l++) {
+        links[l].removeAttribute('rel');
+    }
 }
