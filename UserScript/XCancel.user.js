0945f751e204959bbdf6e1ba9d38dbe87dc4f7e1
Charles Tilford <gitcommit@biocode.fastmail.fm>
2025-06-13 07:12:17 -0400

    Hides tweets below 10 votes

---
+++ UserScript/XCancel.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name          XCancel
+// @namespace     https://github.com/maptracker/confFiles/tree/master/UserScript
+// @match         https://xcancel.com/*
+// @description   Hides low-vote tweets
+// @version       1.0.0
+// @grant         none
+// ==/UserScript==
+
+// Array to hold all comment elements, plus their score
+var voteClass = "icon-heart";
+var minVote = 10;
+
+logX("-- XCancel Tweet Hider --");
+setTimeout(scanStats, 3000);
+
+function scanStats() {
+    var hearts = document.getElementsByClassName(voteClass);
+    for (var i=0; i < hearts.length; i++) {
+        processTweet(hearts[i]);
+    }
+}
+
+function processTweet(el) {
+    if (el.doneAlready) return;
+    el.doneAlready = true;
+    var votes = el.parentNode.innerText;
+    // Set zero for no value
+    if (!votes) votes = "0";
+    // Remove non-numeric, especially ','
+    votes = parseInt(votes.replace(/[^0-9]/g,''));
+    // If we appear to have the minimum number of votes do nothing else
+    if (votes >= minVote) return;
+    // Find appropriate parent node to hide
+    while (1) {
+        el = el.parentNode;
+        if (!el) return; // Somehow failed to find right parent
+        var cn = el.className;
+        if (cn.match("thread-line")) {
+            // This appears to be the main tweet element
+            el.style.display = "none";
+            return;
+        }
+    }
+}
+
+function logX (msg) {
+    console.log(msg);
+}
