eb458602bfa5ef07c49aa112080c39ea145e0fb0
Charles Tilford <gitcommit@biocode.fastmail.fm>
2025-07-11 06:23:25 -0400

    fix error on empty class

--- UserScript/XCancel.user.js
+++ UserScript/XCancel.user.js
@@ -3,22 +3,29 @@
 // @namespace     https://github.com/maptracker/confFiles/tree/master/UserScript
 // @match         https://xcancel.com/*
 // @description   Hides low-vote tweets
-// @version       1.0.0
+// @version       1.0.1
 // @grant         none
 // ==/UserScript==
 
 // Array to hold all comment elements, plus their score
 var voteClass = "icon-heart";
 var minVote = 10;
+var firstScan = true;
 
 logX("-- XCancel Tweet Hider --");
 setTimeout(scanStats, 3000);
 
 function scanStats() {
     var hearts = document.getElementsByClassName(voteClass);
-    for (var i=0; i < hearts.length; i++) {
+    var hl = hearts.length;
+    if (firstScan) {
+        logX("[*] "+hl+ " replies identified on load");
+        firstScan = false;
+    }
+    for (var i=0; i < hl; i++) {
         processTweet(hearts[i]);
     }
+    // setTimeout(scanStats, 3000); // not needed, apparently
 }
 
 function processTweet(el) {
@@ -30,18 +37,26 @@ function processTweet(el) {
     // Remove non-numeric, especially ','
     votes = parseInt(votes.replace(/[^0-9]/g,''));
     // If we appear to have the minimum number of votes do nothing else
-    if (votes >= minVote) return;
+    if (votes >= minVote) {
+        logX("[+] "+votes+ " votes");
+        return;
+    }
     // Find appropriate parent node to hide
     while (1) {
         el = el.parentNode;
-        if (!el) return; // Somehow failed to find right parent
-        var cn = el.className;
+        if (!el) {
+            logX("??? "+votes+ " votes - failed to identify parent");
+            return; // Somehow failed to find right parent
+        }
+        var cn = el.className || "";
         if (cn.match("thread-line")) {
             // This appears to be the main tweet element
             el.style.display = "none";
+            logX("--- "+votes+ " votes");
             return;
         }
     }
+    logX("?!? "+votes+ " votes - generally failed??");
 }
 
 function logX (msg) {
