62a3659d55584ad78d5b8b0a36a627cdf84fa701
Charles Tilford <gitcommit@biocode.fastmail.fm>
2026-09-09 07:01:41 -0400

    size slider + link copy button

--- UserScript/RedditMedia.user.js
+++ UserScript/RedditMedia.user.js
@@ -3,14 +3,17 @@
 // @namespace   https://github.com/maptracker/confFiles/tree/master/UserScript
 // @match       https://www.reddittorjg6rue252oqsxryoxengawnmo46qy4kyii5wtqnwfj4ooad.onion/media*
 // @grant       none
-// @version     1.0.1
+// @version     1.0.2
 // @author      -
 // @description Renames titles to identify "Prove your humanity" tabs
 // ==/UserScript==
+/* jshint esversion: 8 */
 
-var onion="www.reddittorjg6rue252oqsxryoxengawnmo46qy4kyii5wtqnwfj4ooad.onion";
-var com="www.reddit.com";
+const onion="www.reddittorjg6rue252oqsxryoxengawnmo46qy4kyii5wtqnwfj4ooad.onion";
+const com="www.reddit.com";
+const imgClass = 'singleImage';
 isolateImage();
+setStyles();
 renameProveIt();
 
 function isolateImage() {
@@ -28,7 +31,30 @@ function isolateImage() {
   }
   const newImg = document.createElement('img');
   newImg.src = img.src;
+  newImg.className = imgClass;
+  const range = document.createElement("input");
+  range.type = "range";
+  range.min = "1";
+  range.max = "200";
+  range.step = "1";
+  range.value = "100";
+  range.addEventListener("input", () => {
+    resizeSingle(Number(range.value));
+  });
+  const copyLink = document.createElement('button');
+  copyLink.className = "copyLink";
+  copyLink.innerText = "Copy Link";
+  copyLink.style.backgroundColor = "yellow";
+  copyLink.style.borderRadius = "0";
+  copyLink.style.fontSize = "0.6em";
+  copyLink.addEventListener("click", async (event) => {
+    await navigator.clipboard.writeText(newImg.src);
+    event.target.style.backgroundColor = "green";
+  });
   document.body.replaceChildren();
+  document.body.appendChild(range);
+  document.body.appendChild(copyLink);
+  document.body.appendChild(document.createElement('br'));
   document.body.appendChild(newImg);
 }
 
@@ -39,6 +65,17 @@ function renameProveIt() {
   if (t.match("Reddit - Prove your humanity")) {
     document.title = '---';
   }
+}
+function setStyles() {
+  // Supporting zoom slider
+  const singleSize = document.createElement("style");
+  singleSize.id = "singleSize";
+  singleSize.textContent = "." +imgClass + " { width: 100%; }";
+  document.head.appendChild(singleSize);
+}
+function resizeSingle(sz) {
+  const singleSize = document.querySelector("#singleSize");
+  singleSize.textContent = "." +imgClass + ` { width: ${sz}%; }`;
 }
 
 function logX (msg) {
